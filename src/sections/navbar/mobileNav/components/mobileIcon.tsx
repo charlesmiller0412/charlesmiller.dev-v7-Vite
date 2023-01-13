bb83d1f4fb60a4581f3d28ce4cc3028c0da2b006
Charles Miller <charles.miller.0412@gmail.com>
2023-01-13 12:09:54 -0600

    added rounded bg to mobileNav btn's

--- src/sections/navbar/mobileNav/components/mobileIcon.tsx
+++ src/sections/navbar/mobileNav/components/mobileIcon.tsx
@@ -8,23 +8,31 @@ export const MobileIcon = (props: any) => {
 
     return (
         <div className="mobileIcon fixed top-0 left-0 w-full flex justify-between z-50 bg-none h-[5rem] items-center select-none">
-            <div className="mobileIcon__theme pl-[5vw] h-full flex w-[10rem]">
-                {loaded ? <ThemeToggle className="text-lg text-blue" /> : ""}
+            <div className="mobileIcon__theme pl-[5vw] h-full w-[10rem] flex items-center mt-5">
+                <div className="mobileIcon__theme--container flex h-fit bg-black dark:bg-offWhite w-fit p-2 rounded-lg">
+                    {loaded ? (
+                        <ThemeToggle className="text-lg text-blue mx-auto w-[3rem] h-[2.5rem]" />
+                    ) : (
+                        ""
+                    )}
+                </div>
             </div>
-            <div className="mobileIcon__hamburger text-blue text-xl pr-[5vw] z-40">
-                {props.showMenu === false ? (
-                    <FontAwesomeIcon
-                        icon={faBars}
-                        className=" hover:cursor-pointer"
-                        onClick={props.handleClick}
-                    />
-                ) : (
-                    <FontAwesomeIcon
-                        icon={faXmark}
-                        className=" hover:cursor-pointer"
-                        onClick={props.handleClick}
-                    />
-                )}
+            <div className="mobileIcon__hamburger text-blue text-xl pr-[5vw] z-40 mt-5">
+                <div className="mobileIcon__theme--container flex h-fit bg-black dark:bg-offWhite w-fit p-2 rounded-lg">
+                    {props.showMenu === false ? (
+                        <FontAwesomeIcon
+                            icon={faBars}
+                            className=" hover:cursor-pointer w-[3rem] h-[2.5rem]"
+                            onClick={props.handleClick}
+                        />
+                    ) : (
+                        <FontAwesomeIcon
+                            icon={faXmark}
+                            className=" hover:cursor-pointer w-[3rem] h-[2.5rem]"
+                            onClick={props.handleClick}
+                        />
+                    )}
+                </div>
             </div>
         </div>
     );
