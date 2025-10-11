b9d558bf42aa68101bd2207455ed7925997f773a
Charles Miller <charles.miller.0412@gmail.com>
2025-10-11 02:18:41 -0500

    adjust responsiveness of button on hover and style

--- src/sections/navbar/components/resumeBtn.tsx
+++ src/sections/navbar/components/resumeBtn.tsx
@@ -9,8 +9,8 @@ export const ResumeBtn = (props: any) => {
                 );
             }}
             className={
-                props.className +
-                "navbar__right--resume w-52 tablet:w-auto border-2 flex items-center justify-center text-xs h-fit m-auto font-medium transition-all order-0 self-stretch shadow-[-4px_4px_0_0_#12252b] dark:shadow-[-4px_4px_0_0_#efefef] py-[1rem] px-[.5rem] hover:bg-blue"
+                (props.className ? props.className + ' ' : '') +
+                "navbar__right--resume w-[6.5rem] border-2 flex items-center justify-center text-xs h-fit font-medium transition-all order-0 self-stretch shadow-[-4px_4px_0_0_#12252b] dark:shadow-[-4px_4px_0_0_#efefef] py-[1rem] px-[.5rem] hover:bg-blue tracking-[0.1rem] mx-auto"
             }
         >
             R&egrave;sum&egrave;
